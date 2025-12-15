f3af42b74c567d57ef9e2cab7456c9f94a275f72
loricaudin <146658637+loricaudin@users.noreply.github.com>
2025-12-15 20:06:56 +0100

    V1.9.1 Minicorrection contact

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -10,7 +10,7 @@ export default function Contact() {
                 <ul>
                     <li>Adresse mail : <a href="mailto:loricaudin@yahoo.fr">loricaudin@yahoo.fr</a></li>
                     <li>Compte LinkedIn : <a href="https://www.linkedin.com/in/loric-audin-1a1b29250/">https://www.linkedin.com/in/loric-audin-1a1b29250/</a></li>
-                    <li>Numéro de téléphone : <a href="tel:+33644764441">06 44 76 44 41</a></li>
+                    { /* <li>Numéro de téléphone : <a href="tel:+33644764441">06 44 76 44 41</a></li> */ }
                 </ul>
             </div>
         </Section>
